26a7849995e796a1e7e0ee77306046a8aab6e901
Maxim Uryumtsev <uryumtsevm@gmail.com>
2025-09-15 02:03:07 +0700

    App not JSX

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,49 +1,60 @@
-import { useState } from "react";
+import { useState, createElement } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-	{
-		/* Декларативно создаём состояние, переменную*/
-	}
 	const [count, setCount] = useState(0);
 	const currentYear = new Date().getFullYear();
 
-	return (
-		<>
-			{" "}
-			{/* Декларативно описываем UI */}
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				{/* Декларативно описываем поведение кнопки через событие */}
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			<footer>
-				{/* Декларативно выводим текущий год */}
-				<p>{currentYear}</p>
-			</footer>
-		</>
+	return createElement(
+		"div",
+		null,
+		createElement(
+			"div",
+			null,
+			createElement(
+				"a",
+				{ href: "https://vite.dev", target: "_blank" },
+				createElement("img", {
+					src: viteLogo,
+					className: "logo",
+					alt: "Vite logo",
+				}),
+			),
+			createElement(
+				"a",
+				{ href: "https://react.dev", target: "_blank" },
+				createElement("img", {
+					src: reactLogo,
+					className: "logo react",
+					alt: "React logo",
+				}),
+			),
+		),
+		createElement("h1", null, "Vite + React"),
+		createElement(
+			"div",
+			{ className: "card" },
+			createElement(
+				"button",
+				{ onClick: () => setCount((c) => c + 1) },
+				`count is ${count}`,
+			),
+			createElement(
+				"p",
+				null,
+				"Edit ",
+				createElement("code", null, "src/App.jsx"),
+				" and save to test HMR",
+			),
+		),
+		createElement(
+			"p",
+			{ className: "read-the-docs" },
+			"Click on the Vite and React logos to learn more",
+		),
+		createElement("footer", null, createElement("p", null, currentYear)),
 	);
 }
 
